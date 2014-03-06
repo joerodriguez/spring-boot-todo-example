7bd491ea91ee84f371fb236db403d15e893a2d0c
Joseph Rodriguez <joerodriguez@gmail.com>
2014-03-05 19:52:04 -0700

    TODO's for upgrading dependencies

--- src/main/java/com/github/joerodriguez/springbootexample/Security.java
+++ src/main/java/com/github/joerodriguez/springbootexample/Security.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -55,7 +54,7 @@ public class Security extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable() // TODO remove when thymeleaf is upgraded
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/todo-list")
